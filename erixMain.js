f9e0393e89722de1b9042310a87bfa472f126296
Eric Wong <cuc.eric@gmail.com>
2020-10-06 07:19:26 +0800

    Update erixMain.js

--- erixMain.js
+++ erixMain.js
@@ -1,10 +1,33 @@
 const moment = require("moment");
+const erixDNSPod = require('./erixDNSPod');
 
-var logPath = './log/erixLog_'+moment().format('yyyyMMDD') + '_' + moment().format('HHmmss')+'.log';
-var dp = require('./erixDNSPod').erixDNSPod(logPath);
+var log = {
+    path: './log/erixLog_' + moment().format('yyyyMMDD') + '_' + moment().format('HHmmss') + '.log',
+    show: true
+};
+
+var working = false;
 
 //Check && update IP Address
+
+var dp = new erixDNSPod.erixDNSPod(log);
+
+setInterval(async function () {
+
+    if (working) { return false; }
+    console.log('Begin');
+    working = true;
+    await dp.update();
+    working = false;
+    console.log('End');
+
+}, dp.sys.conf.interval * 1000 * 60);
+
+
 //Check mail
 //Upload frp config
 
-setInterval(dp.run, dp.sys.conf.interval * 1000 * 60);
+//Set a timmer to check mail
+//if there's new mail with right subject but can't parse, send a how-to mail
+
+
